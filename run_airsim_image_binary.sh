72e7b2fae71774a4b4a00ee7930c4e7e72ab008f
GlazerMann <kurt@ioiowa.com>
2022-10-26 14:35:11 -0500

    Use DISPLAY instead of assuming :0

--- run_airsim_image_binary.sh
+++ run_airsim_image_binary.sh
@@ -11,7 +11,7 @@ UNREAL_BINARY_SHELL_ABSPATH=$(readlink -f $2)
 XAUTH=/tmp/.docker.xauth
 if [ ! -f $XAUTH ]
 then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    xauth_list=$(xauth nlist ${DISPLAY} | sed -e 's/^..../ffff/')
     if [ ! -z "$xauth_list" ]
     then
         echo $xauth_list | xauth -f $XAUTH nmerge -
